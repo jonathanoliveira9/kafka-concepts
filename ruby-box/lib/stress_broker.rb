cf6319fc37ec9bb7318bfe9ad612586a92e470e6
Jonathan Oliveira <jonathanoliveirasilva9@gmail.com>
2025-03-08 03:18:45 +0000

    add benchmark to stress broker

--- ruby-box/lib/stress_broker.rb
+++ ruby-box/lib/stress_broker.rb
@@ -1,37 +1,54 @@
 require 'kafka'
 require 'concurrent-ruby'
+require 'benchmark'
 
 class StressBroker
-  def initialize(num_messages = 500_000, num_threads = 20)
+  def initialize(num_messages = 1_000, num_threads = 20)
     @num_messages = num_messages
     @num_threads = num_threads
   end
 
-  def execute
-    puts "Initializing stress on Kafka #{@num_messages} messages..."
-    stress_test(producer, 'strees-benchmarking')
+  def execute_benchmark
+    Benchmark.bmbm do |x|
+      x.report("protobuf") { stress_protobuf }
+      x.report("json") { stress_json }
+    end
+  end
 
-    puts "Finished!"
+  def execute
+    stress_json
   end
 
   private
 
-  def producer
-    @producer = kafka.async_producer(
-      max_queue_size: 1_000_000,
-      delivery_threshold: 100, 
-      delivery_interval: 0.2,
-      compression_codec: :snappy)
+  def topic
+    'test-topic01-protobuf'
   end
 
-  def stress_test(producer, topic)
+  def message
+    @message ||= Array.new(10 * 1024) { rand(65..90).chr }.join
+  end
+
+  def stress_json
+    message_json = { id: 1, name: message }
+
+    stress_test(message_json)
+  end
+
+  def stress_protobuf
+    message_protobuf = Example::User.new(id: 1, name: message)
+
+    encoded = Example::User.encode(message_protobuf)
+
+    stress_test(encoded)
+  end
+
+  def stress_test(message)
     pool = Concurrent::FixedThreadPool.new(@num_threads)
 
     @num_messages.times do |i|
       pool.post do 
         key = "key-#{i % 1000 }"
-        message = { message: Time.now.to_f }
-
         begin
           producer.produce(message, topic: topic, partition_key: key)
         rescue => e
@@ -46,7 +63,22 @@ class StressBroker
     producer.shutdown
   end
 
+  def producer
+    @producer = kafka.async_producer(
+      max_queue_size: 1_000_000,
+      delivery_threshold: 100, 
+      delivery_interval: 0.2,
+      compression_codec: :snappy
+    )
+  end
+
   def kafka
-    @kafka ||= Kafka.new(["kafka1:9092", "kafka2:9093", "kafka3:9094"], client_id: "stress-tester")
+    @kafka ||= Kafka.new(
+      ["kafka1:9092", "kafka2:9093", "kafka3:9094"],
+      client_id: "stress-tester"
+      # required_acks: 0, At most once
+      # required_acks: 1 at least once
+      # required_acks: "all" exactly once
+    )
   end
 end
